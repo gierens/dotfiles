d2c7d50c17147f37d035d65cb2b4e1f73e92a270
Sandro-Alessio Gierens <sandro@gierens.de>
2024-11-11 09:15:07 +0100

    feat(nvim/keymaps): add jj as esc alternative
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+
+-- esc
+map("i", "jj", "<esc>", { desc = "Quit insert mode" })
+map("v", "jj", "<esc>", { desc = "Quit insert mode" })

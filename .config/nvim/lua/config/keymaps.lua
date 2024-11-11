3a8376831fe709ad7f552d5aef0013530cab90e1
Sandro-Alessio Gierens <sandro@gierens.de>
2024-11-11 09:15:40 +0100

    feat(nvim/keymaps): add basic fugitive mappings
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,10 @@ local map = LazyVim.safe_keymap_set
 -- esc
 map("i", "jj", "<esc>", { desc = "Quit insert mode" })
 map("v", "jj", "<esc>", { desc = "Quit insert mode" })
+
+-- fugitive
+map("n", "<leader>ga", "<cmd>Git add %<cr>", { desc = "Stage current file" })
+map("n", "<leader>gA", "<cmd>Git add .<cr>", { desc = "Stage all changes" })
+map("n", "<leader>gg", "<cmd>Git commit -sS<cr>", { desc = "Commit staged changes" })
+map("n", "<leader>gp", "<cmd>Git push<cr>", { desc = "Push to remote" })
+map("n", "<leader>gP", "<cmd>Git pull<cr>", { desc = "Pull from remote" })

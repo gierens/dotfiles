5e9772c9abeb8dbf5b3e464c7e9a5e69900c0127
Sandro-Alessio Gierens <sandro@gierens.de>
2024-12-21 10:15:56 +0100

    feat(nvim/plugins/extras): import lang.zig
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- .config/nvim/lua/plugins/extras.lua
+++ .config/nvim/lua/plugins/extras.lua
@@ -1,5 +1,6 @@
 return {
   { import = "lazyvim.plugins.extras.lang.clangd" },
   { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.lang.zig" },
   { import = "lazyvim.plugins.extras.ai.copilot" },
 }

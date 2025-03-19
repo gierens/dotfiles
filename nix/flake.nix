4c116d1b89304625f93dab71b0defabf3e3b2125
Sandro-Alessio Gierens <sandro@gierens.de>
2025-03-19 15:15:31 +0000

    feat(nix/flake): add homeConfigurations again
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- nix/flake.nix
+++ nix/flake.nix
@@ -68,15 +68,15 @@
 
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
-    # homeConfigurations = {
-    #   "sandro@garrus" = home-manager.lib.homeManagerConfiguration {
-    #     pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-    #     extraSpecialArgs = {inherit inputs outputs;};
-    #     modules = [
-    #       # > Our main home-manager configuration file <
-    #       ./home-manager/home.nix
-    #     ];
-    #   };
-    # };
+    homeConfigurations = {
+      "sandro@garrus" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+        modules = [
+          # > Our main home-manager configuration file <
+          ./home-manager/home.nix
+        ];
+      };
+    };
   };
 }

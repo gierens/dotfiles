4db72215d9fecbbd8f55e3c15f17f4be2f746c20
Sandro-Alessio Gierens <sandro@gierens.de>
2025-03-19 16:04:16 +0000

    feat(nix/home): add nix-tree package
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -97,6 +97,7 @@
     # it provides the command `nom` works just like `nix`
     # with more details log output
     nix-output-monitor
+    nix-tree
 
     # productivity
     hugo # static site generator

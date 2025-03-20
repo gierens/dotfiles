eea442f9ba5d7370f97a912210cb9965c190e419
Sandro-Alessio Gierens <sandro@gierens.de>
2025-03-20 13:25:28 +0000

    feat(nix): add citadel host
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- nix/flake.nix
+++ nix/flake.nix
@@ -64,12 +64,27 @@
           ./hosts/garrus/configuration.nix
         ];
       };
+      citadel = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          # > Our main nixos configuration file <
+          ./hosts/citadel/configuration.nix
+        ];
+      };
     };
 
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
       "sandro@garrus" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+        modules = [
+          # > Our main home-manager configuration file <
+          ./home-manager/home.nix
+        ];
+      };
+      "sandro@citadel" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         modules = [

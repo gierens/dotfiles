331b4bf5621219fb8c974cf2937f61fdd4477a87
Sandro-Alessio Gierens <sandro@gierens.de>
2025-03-20 13:10:47 +0000

    refactor(nix): move garrus config to hosts/garrus
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- nix/flake.nix
+++ nix/flake.nix
@@ -61,7 +61,7 @@
         specialArgs = {inherit inputs outputs;};
         modules = [
           # > Our main nixos configuration file <
-          ./nixos/configuration.nix
+          ./hosts/garrus/configuration.nix
         ];
       };
     };

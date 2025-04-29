245d02f4ef616ff38f2e7e90909e8a19c27d99e3
Sandro-Alessio Gierens <sandro@gierens.de>
2025-04-29 23:09:54 +0200

    feat(nix/hosts/liara): activate nix-command and flakes
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- nix/hosts/liara/configuration.nix
+++ nix/hosts/liara/configuration.nix
@@ -56,24 +56,24 @@
   #   };
   # };
 
-  # nix = let
-  #   flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
-  # in {
-  #   settings = {
-  #     # Enable flakes and new 'nix' command
-  #     experimental-features = "nix-command flakes";
-  #     # Opinionated: disable global registry
-  #     flake-registry = "";
-  #     # Workaround for https://github.com/NixOS/nix/issues/9574
-  #     nix-path = config.nix.nixPath;
-  #   };
-  #   # Opinionated: disable channels
-  #   # channel.enable = false;
+  nix = let
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
+    settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+      # Opinionated: disable global registry
+      # flake-registry = "";
+      # Workaround for https://github.com/NixOS/nix/issues/9574
+      # nix-path = config.nix.nixPath;
+    };
+    # Opinionated: disable channels
+    # channel.enable = false;
 
-  #   # Opinionated: make flake registry and nix path match flake inputs
-  #   registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-  #   nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
-  # };
+    # Opinionated: make flake registry and nix path match flake inputs
+    # registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    # nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+  };
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;

6b614651b987fa2f2c6ca56f505932ccbc9449ee
Sandro-Alessio Gierens <sandro@gierens.de>
2025-05-19 11:54:11 +0200

    feat(home): add .cargo/bin to bash path
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- home/home.nix
+++ home/home.nix
@@ -217,7 +217,7 @@
     enableCompletion = true;
     # TODO add your custom bashrc here
     bashrcExtra = ''
-      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/.cargo/bin"
 
       view_pdf() {
           if [ -z "$1" ]; then

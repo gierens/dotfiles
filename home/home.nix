37efd71de559ec10bff36a1c156564cabf924667
Sandro-Alessio Gierens <sandro@gierens.de>
2025-05-25 11:00:37 +0200

    refactor(home): move dconf settings to own mod
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- home/home.nix
+++ home/home.nix
@@ -19,6 +19,7 @@
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
     ./alacritty.nix
+    ./dconf.nix
     ./git.nix
     ./tmux.nix
   ];
@@ -50,41 +51,6 @@
   home = {
     username = "sandro";
     homeDirectory = "/home/sandro";
-  };
-
-  dconf.settings = {
-    "org/gnome/mutter" = {
-      workspaces-only-on-primary = false;
-    };
-    "org/gnome/desktop/wm/keybindings" = {
-      close = [ "<Ctrl>q" ];
-      switch-applications = [];
-      switch-applications-backward = [];
-      switch-windows = [ "<Super>Tab" ];
-      switch-windows-backward = [ "<Super><Shift>Tab" ];
-    };
-    "org/gnome/settings-daemon/plugins/media-keys" = {
-      custom-keybindings = [
-        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal/"
-        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/files/"
-        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/browser/"
-      ];
-    };
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/terminal" = {
-      binding = "<Super>t";
-      command = "alacritty";
-      name = "terminal";
-    };
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/files" = {
-      binding = "<Super>f";
-      command = "nautilus";
-      name = "files";
-    };
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/browser" = {
-      binding = "<Super>b";
-      command = "chromium";
-      name = "browser";
-    };
   };
 
   # Add stuff for your user as you see fit:

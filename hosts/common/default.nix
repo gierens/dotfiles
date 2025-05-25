f846e9f59d0d30f6184c0bfa3918c01d6689b860
Sandro-Alessio Gierens <sandro@gierens.de>
2025-05-25 14:23:16 +0200

    docs(hosts/common): add todo regarding moving texlive
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -20,6 +20,7 @@
   environment.systemPackages = with pkgs; [
     pinentry-curses
     wireguard-tools
+    # TODO: this should not be installed by default
     (pkgs.texlive.combine {
     inherit (pkgs.texlive)
       scheme-full

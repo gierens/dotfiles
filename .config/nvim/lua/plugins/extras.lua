67e441aad0c0b9d011df40059a5f86624ecb60b5
Sandro-Alessio Gierens <sandro@gierens.de>
2025-07-24 19:31:57 +0200

    feat(nvim): install editor plugin leap
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- .config/nvim/lua/plugins/extras.lua
+++ .config/nvim/lua/plugins/extras.lua
@@ -4,5 +4,6 @@ return {
   { import = "lazyvim.plugins.extras.lang.rust" },
   { import = "lazyvim.plugins.extras.lang.zig" },
   { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.editor.leap" },
   # { import = "lazyvim.plugins.extras.ai.copilot" },
 }

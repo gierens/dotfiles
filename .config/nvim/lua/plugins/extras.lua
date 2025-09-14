b28b3faf36bc82fdf3c4be3f6096d6241b1f793c
Sandro-Alessio Gierens <sandro@gierens.de>
2025-09-14 12:29:25 +0200

    feat(config/nvim): add plugin extras.lang.tex
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- .config/nvim/lua/plugins/extras.lua
+++ .config/nvim/lua/plugins/extras.lua
@@ -4,6 +4,7 @@ return {
   { import = "lazyvim.plugins.extras.lang.rust" },
   { import = "lazyvim.plugins.extras.lang.zig" },
   { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.tex" },
   { import = "lazyvim.plugins.extras.editor.leap" },
   -- { import = "lazyvim.plugins.extras.ai.copilot" },
 }

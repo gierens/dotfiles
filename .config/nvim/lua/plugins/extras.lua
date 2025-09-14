c6f3e943cd4859cf54c17286a7c5f95d1e93fa65
Sandro-Alessio Gierens <sandro@gierens.de>
2025-09-14 12:30:11 +0200

    feat(config/nvim): add and comment out extras.coding.nvim-cmp
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- .config/nvim/lua/plugins/extras.lua
+++ .config/nvim/lua/plugins/extras.lua
@@ -5,6 +5,7 @@ return {
   { import = "lazyvim.plugins.extras.lang.zig" },
   { import = "lazyvim.plugins.extras.lang.python" },
   { import = "lazyvim.plugins.extras.lang.tex" },
+  -- { import = "lazyvim.plugins.extras.coding.nvim-cmp" },
   { import = "lazyvim.plugins.extras.editor.leap" },
   { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
   -- { import = "lazyvim.plugins.extras.ai.copilot" },

a0e6db11046f9de18110262861f2b3a79ee260c2
Sandro-Alessio Gierens <sandro@gierens.de>
2026-01-02 00:58:21 +0100

    feat(hosts/reaper): make sandro a trusted nix user
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- hosts/reaper/configuration.nix
+++ hosts/reaper/configuration.nix
@@ -66,6 +66,8 @@
       flake-registry = "";
       # Workaround for https://github.com/NixOS/nix/issues/9574
       nix-path = config.nix.nixPath;
+
+      trusted-users = [ "root" "sandro" ];
     };
     # Opinionated: disable channels
     # channel.enable = false;

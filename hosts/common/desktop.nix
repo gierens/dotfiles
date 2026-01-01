f82437058adfedfe3d60fe8dd773f67af5bcb9e2
Sandro-Alessio Gierens <sandro@gierens.de>
2026-01-02 00:45:42 +0100

    feat(hosts/desktop): use alacritty-graphics instead of alacritty package
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- hosts/common/desktop.nix
+++ hosts/common/desktop.nix
@@ -48,7 +48,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    alacritty
+    unstable.alacritty-graphics
     kitty
     ghostty
     networkmanagerapplet

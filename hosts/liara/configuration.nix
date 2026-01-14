20110a73398b1ee0aae316cfc831a9b57ee66c46
Sandro-Alessio Gierens <sandro@gierens.de>
2026-01-14 22:56:24 +0100

    fix(hosts/liara): re-enable overlays
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- hosts/liara/configuration.nix
+++ hosts/liara/configuration.nix
@@ -31,30 +31,30 @@
     inputs.home-manager.nixosModules.home-manager
   ];
 
-  # nixpkgs = {
-  #   # You can add overlays here
-  #   overlays = [
-  #     # Add overlays your own flake exports (from overlays and pkgs dir):
-  #     outputs.overlays.additions
-  #     outputs.overlays.modifications
-  #     outputs.overlays.unstable-packages
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
 
-  #     # You can also add overlays exported from other flakes:
-  #     # neovim-nightly-overlay.overlays.default
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
 
-  #     # Or define it inline, for example:
-  #     # (final: prev: {
-  #     #   hi = final.hello.overrideAttrs (oldAttrs: {
-  #     #     patches = [ ./change-hello-to-hi.patch ];
-  #     #   });
-  #     # })
-  #   ];
-  #   # Configure your nixpkgs instance
-  #   config = {
-  #     # Disable if you don't want unfree packages
-  #     allowUnfree = true;
-  #   };
-  # };
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
 
   nix =
     # let flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs; in 

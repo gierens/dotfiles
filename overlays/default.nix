78b124ec60555fc50ae3d0bb56c54459df0b4602
Sandro-Alessio Gierens <sandro@gierens.de>
2026-06-08 16:03:12 +0200

    style(overlays): apply auto-formatting
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,6 @@
 # This file defines overlays
-{inputs, ...}: {
+{ inputs, ... }:
+{
   # This one brings our custom packages from the 'pkgs' directory
   additions = final: _prev: import ../pkgs final.pkgs;
 
@@ -11,13 +12,15 @@
     # ...
     # });
     weechat = prev.weechat.override {
-      configure = { availablePlugins, ... }: {
-        scripts = with prev.weechatScripts; [
-          wee-slack
-        ];
-        # Uncomment this if you're on Darwin, there's no PHP support available. See https://github.com/NixOS/nixpkgs/blob/e6bf74e26a1292ca83a65a8bb27b2b22224dcb26/pkgs/applications/networking/irc/weechat/wrapper.nix#L13 for more info.
-        # plugins = builtins.attrValues (builtins.removeAttrs availablePlugins [ "php" ]);
-      };
+      configure =
+        { availablePlugins, ... }:
+        {
+          scripts = with prev.weechatScripts; [
+            wee-slack
+          ];
+          # Uncomment this if you're on Darwin, there's no PHP support available. See https://github.com/NixOS/nixpkgs/blob/e6bf74e26a1292ca83a65a8bb27b2b22224dcb26/pkgs/applications/networking/irc/weechat/wrapper.nix#L13 for more info.
+          # plugins = builtins.attrValues (builtins.removeAttrs availablePlugins [ "php" ]);
+        };
     };
   };
 

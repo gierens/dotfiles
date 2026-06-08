a4e44c50b0c3e1ce657f3b73fb16d47081918640
Sandro-Alessio Gierens <sandro@gierens.de>
2026-06-08 16:05:30 +0200

    style(home): apply auto-formatting
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- home/home.nix
+++ home/home.nix
@@ -56,209 +56,212 @@
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
-  home.packages = with pkgs; [
-    unstable.tree-sitter
-    unstable.neovim
+  home.packages =
+    with pkgs;
+    [
+      unstable.tree-sitter
+      unstable.neovim
 
-    # TODO: this doesn't work on 26.05 anymore, look for alternative
-    # neofetch
-    nnn # terminal file manager
+      # TODO: this doesn't work on 26.05 anymore, look for alternative
+      # neofetch
+      nnn # terminal file manager
 
-    # archives
-    zip
-    xz
-    unzip
-    p7zip
+      # archives
+      zip
+      xz
+      unzip
+      p7zip
 
-    # utils
-    bat # cat alternative
-    delta # diff alternative
-    unstable.eza # ls alternative
-    fd # find alternative
-    fzf # fuzzy finder
-    hyperfine # cli benchmark tool
-    jq # json processor
-    just # make alternative
-    ripgrep # grep alternative
-    tokei # count loc
-    xh # http client
-    yq-go # yaml processor
-    xxd # hex and binary dump utitlity
+      # utils
+      bat # cat alternative
+      delta # diff alternative
+      unstable.eza # ls alternative
+      fd # find alternative
+      fzf # fuzzy finder
+      hyperfine # cli benchmark tool
+      jq # json processor
+      just # make alternative
+      ripgrep # grep alternative
+      tokei # count loc
+      xh # http client
+      yq-go # yaml processor
+      xxd # hex and binary dump utitlity
 
-    # cli clients
-    ncspot # spotify tui
-    wiki-tui # wikipedia tui
+      # cli clients
+      ncspot # spotify tui
+      wiki-tui # wikipedia tui
 
-    # networking tools
-    mtr # A network diagnostic tool
-    iperf3
-    dnsutils # `dig` + `nslookup`
-    ldns # replacement of `dig`, it provide the command `drill`
-    aria2 # A lightweight multi-protocol & multi-source command-line download utility
-    socat # replacement of openbsd-netcat
-    nmap # A utility for network discovery and security auditing
-    ipcalc # it is a calculator for the IPv4/v6 addresses
+      # networking tools
+      mtr # A network diagnostic tool
+      iperf3
+      dnsutils # `dig` + `nslookup`
+      ldns # replacement of `dig`, it provide the command `drill`
+      aria2 # A lightweight multi-protocol & multi-source command-line download utility
+      socat # replacement of openbsd-netcat
+      nmap # A utility for network discovery and security auditing
+      ipcalc # it is a calculator for the IPv4/v6 addresses
 
-    # nix related
-    #
-    # it provides the command `nom` works just like `nix`
-    # with more details log output
-    nix-output-monitor
-    nix-tree
-    # NOTE: could be relevant at some point
-    # nix-prefetch-git
-    # nix-prefetch-github
+      # nix related
+      #
+      # it provides the command `nom` works just like `nix`
+      # with more details log output
+      nix-output-monitor
+      nix-tree
+      # NOTE: could be relevant at some point
+      # nix-prefetch-git
+      # nix-prefetch-github
 
-    # productivity
-    hugo # static site generator
-    glow # markdown previewer in terminal
-    pandoc
-    unstable.presenterm # tui markdown presentations
-    python312Packages.weasyprint
+      # productivity
+      hugo # static site generator
+      glow # markdown previewer in terminal
+      pandoc
+      unstable.presenterm # tui markdown presentations
+      python312Packages.weasyprint
 
-    htop
-    btop-cuda # replacement of htop/nmon
-    iotop # io monitoring
-    iftop # network monitoring
+      htop
+      btop-cuda # replacement of htop/nmon
+      iotop # io monitoring
+      iftop # network monitoring
 
-    # system call monitoring
-    strace # system call monitoring
-    ltrace # library call monitoring
-    lsof # list open files
+      # system call monitoring
+      strace # system call monitoring
+      ltrace # library call monitoring
+      lsof # list open files
 
-    # system tools
-    sysstat
-    lm_sensors # for `sensors` command
-    ethtool
-    pciutils # lspci
-    usbutils # lsusb
-    ncdu
-    smartmontools
-    dmidecode
-    msr-tools
+      # system tools
+      sysstat
+      lm_sensors # for `sensors` command
+      ethtool
+      pciutils # lspci
+      usbutils # lsusb
+      ncdu
+      smartmontools
+      dmidecode
+      msr-tools
 
-    # rust related
-    unstable.cargo-machete
-    unstable.cargo-spellcheck
-    unstable.mdbook
-    unstable.rustup
-    unstable.sccache
-    unstable.sqlx-cli
-    unstable.rusty-man # rust docs as man page
-    unstable.cargo-expand
-    dioxus-cli
-    # NOTE: these we currently install directly via cargo install to get the latest version
-    # unstable.cargo-audit
-    # unstable.cargo-deny
-    # wasm-bindgen-cli
-    unstable.leetcode-cli
-    cargo-leet
-    unstable.cargo-bootimage
+      # rust related
+      unstable.cargo-machete
+      unstable.cargo-spellcheck
+      unstable.mdbook
+      unstable.rustup
+      unstable.sccache
+      unstable.sqlx-cli
+      unstable.rusty-man # rust docs as man page
+      unstable.cargo-expand
+      dioxus-cli
+      # NOTE: these we currently install directly via cargo install to get the latest version
+      # unstable.cargo-audit
+      # unstable.cargo-deny
+      # wasm-bindgen-cli
+      unstable.leetcode-cli
+      cargo-leet
+      unstable.cargo-bootimage
 
-    # verus related
-    z3_4_12_5
-    # rustup # see above
-    verus
-    verus-analyzer
+      # verus related
+      z3_4_12_5
+      # rustup # see above
+      verus
+      verus-analyzer
 
-    # misc
-    cowsay
-    file
-    which
-    tree
-    gnused
-    gnutar
-    gawk
-    zstd
-    gnupg
-    pinentry-all
-    unstable.codecrafters-cli
-    stdenv
-    ncurses
-    stow
-    gnumake
-    pass
-    pwgen
-    aerc
-    aba
-    # browserpass
-    brave
-    zsh-z
-    zsh-syntax-highlighting
-    zsh-autosuggestions
-    gh
-    evince
-    slack-cli
-    (lib.hiPrio gcc)
-    clang
-    clang-tools
-    gdb
-    gef
-    nasm
-    nil
-    mold
-    mariadb.client
-    llvm
-    libllvm
-    lldb
-    pyright
-    ruff
-    openstackclient-full
-    python3
-    unstable.zig_0_14
-    zls
-    qemu
-    OVMF
-    xorriso
-    killall
-    bc
-    dpkg
-    catimg
-    bunyan-rs
-    tcpdump
-    iamb
-    yt-dlp
-    openldap
-    openssl
-    typst
-    qrrs
-    xournalpp
-    unstable.stockfish
-    unstable.chess-tui
-    weechat
-    unstable.calcurse
-    gcalcli
-    khal
-    vdirsyncer
+      # misc
+      cowsay
+      file
+      which
+      tree
+      gnused
+      gnutar
+      gawk
+      zstd
+      gnupg
+      pinentry-all
+      unstable.codecrafters-cli
+      stdenv
+      ncurses
+      stow
+      gnumake
+      pass
+      pwgen
+      aerc
+      aba
+      # browserpass
+      brave
+      zsh-z
+      zsh-syntax-highlighting
+      zsh-autosuggestions
+      gh
+      evince
+      slack-cli
+      (lib.hiPrio gcc)
+      clang
+      clang-tools
+      gdb
+      gef
+      nasm
+      nil
+      mold
+      mariadb.client
+      llvm
+      libllvm
+      lldb
+      pyright
+      ruff
+      openstackclient-full
+      python3
+      unstable.zig_0_14
+      zls
+      qemu
+      OVMF
+      xorriso
+      killall
+      bc
+      dpkg
+      catimg
+      bunyan-rs
+      tcpdump
+      iamb
+      yt-dlp
+      openldap
+      openssl
+      typst
+      qrrs
+      xournalpp
+      unstable.stockfish
+      unstable.chess-tui
+      weechat
+      unstable.calcurse
+      gcalcli
+      khal
+      vdirsyncer
 
-    # NOTE: might be relevant later
-    # taskwarrior3
-    # unstable.todoist
+      # NOTE: might be relevant later
+      # taskwarrior3
+      # unstable.todoist
 
-    # TODO: suggestions:
-    # - fish
-    # - nushell
-    # - zoxide # cd alternative
-    # - zellij  # tmux alternative
-    # - gitui # git tui
-    # - du-dust # du alternative
-    # - dua # du alternative
-    # - yazi # terminal file manager
-    # - evil-helix # helix with vim bindings
-    # - bacon   # background code checker
-    # - cargo-info # show crate information
-    # - fselect # find with SQL like queries
-    # - ripgrep-all
-    # - mask # make alternative
-    # - mprocs # command parallelization
-    # - kondo # build artifact cleaner
-    # - bob-nvim # neovim version manager
-    # - rtx # some runtime???
-    # - espanso # text expander
-  ] ++ lib.optionals (pkgs.stdenv.hostPlatform.system == "x86_64-linux") [
-    arduino-cli
-    arduino-ide
-  ];
+      # TODO: suggestions:
+      # - fish
+      # - nushell
+      # - zoxide # cd alternative
+      # - zellij  # tmux alternative
+      # - gitui # git tui
+      # - du-dust # du alternative
+      # - dua # du alternative
+      # - yazi # terminal file manager
+      # - evil-helix # helix with vim bindings
+      # - bacon   # background code checker
+      # - cargo-info # show crate information
+      # - fselect # find with SQL like queries
+      # - ripgrep-all
+      # - mask # make alternative
+      # - mprocs # command parallelization
+      # - kondo # build artifact cleaner
+      # - bob-nvim # neovim version manager
+      # - rtx # some runtime???
+      # - espanso # text expander
+    ]
+    ++ lib.optionals (pkgs.stdenv.hostPlatform.system == "x86_64-linux") [
+      arduino-cli
+      arduino-ide
+    ];
 
   # Let home Manager install and manage itself.
   programs.home-manager.enable = true;

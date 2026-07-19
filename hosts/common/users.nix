9f03d3c9c27a94b2e644f10c8516e71ead086e37
Sandro-Alessio Gierens <sandro@gierens.de>
2026-07-19 17:36:05 +0200

    style(hosts/users): re-apply auto-formatting
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- hosts/common/users.nix
+++ hosts/common/users.nix
@@ -1,7 +1,8 @@
 {
   pkgs,
   ...
-}: {
+}:
+{
   users.users.sandro = {
     isNormalUser = true;
     extraGroups = [
@@ -14,7 +15,9 @@
     # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
     # Be sure to change it (using passwd) after rebooting!
     # initialPassword = "correcthorsebatterystaple";
-    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBevyJ5i0237DNoS29F9aii2AJwrSxXNz3hP61hWXfRl sandro@reaper.gierens.de" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBevyJ5i0237DNoS29F9aii2AJwrSxXNz3hP61hWXfRl sandro@reaper.gierens.de"
+    ];
     # packages = with pkgs; [
     #   # tree
     # ];

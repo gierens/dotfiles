99477987d8bd0ff9e670f031c399cb66ad7fdea1
Sandro-Alessio Gierens <sandro@gierens.de>
2026-09-13 00:12:04 +0200

    refactor(home): move starship config to own module
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- home/home.nix
+++ home/home.nix
@@ -23,6 +23,7 @@
     ./dconf.nix
     ./git.nix
     ./tmux.nix
+    ./starship.nix
   ];
 
   nixpkgs = {
@@ -267,18 +268,6 @@
 
   # Let home Manager install and manage itself.
   programs.home-manager.enable = true;
-
-  # starship - an customizable prompt for any shell
-  programs.starship = {
-    enable = true;
-    # custom settings
-    settings = {
-      add_newline = false;
-      aws.disabled = true;
-      gcloud.disabled = true;
-      line_break.disabled = true;
-    };
-  };
 
   home.file."${config.home.homeDirectory}/.config/nvim".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/projects/dotfiles/.config/nvim";

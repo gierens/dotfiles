81656e2f1d406e832118ccc265920a52535638d5
Sandro-Alessio Gierens <sandro@gierens.de>
2026-09-13 00:13:10 +0200

    feat(home): install traceroute
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- home/home.nix
+++ home/home.nix
@@ -102,6 +102,7 @@
       socat # replacement of openbsd-netcat
       nmap # A utility for network discovery and security auditing
       ipcalc # it is a calculator for the IPv4/v6 addresses
+      traceroute
 
       # nix related
       #

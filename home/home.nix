c7da503b1cde26561620150862fff80d2f18d945
Sandro-Alessio Gierens <sandro@gierens.de>
2026-09-13 10:46:56 +0200

    chore(home): update home.stateVersion
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- home/home.nix
+++ home/home.nix
@@ -295,5 +295,5 @@
   # You can update home Manager without changing this value. See
   # the home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "24.11";
+  home.stateVersion = "26.05";
 }

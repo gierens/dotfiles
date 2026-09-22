ac85c19d80d039e7940476bfbb4e3c65d94cc192
Sandro-Alessio Gierens <sandro@gierens.de>
2026-09-22 12:34:20 +0200

    feat(nvim/keymaps): remap diagraph insert to C-l
    blink.cmp takes over C-k and reconfiguring that seems to be much more
    tedious than this.
    
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -13,3 +13,6 @@ map("n", "<leader>gA", "<cmd>Git add .<cr>", { desc = "Stage all changes" })
 map("n", "<leader>gg", "<cmd>Git commit -sS<cr>", { desc = "Commit staged changes" })
 map("n", "<leader>gp", "<cmd>Git push<cr>", { desc = "Push to remote" })
 map("n", "<leader>gP", "<cmd>Git pull<cr>", { desc = "Pull from remote" })
+
+-- remap diagraph insertion (blink.cmp takes over <C-k>)
+map("i", "<C-l>", "<C-k>", { noremap = true })
